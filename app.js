cc089107776223d8556e3893b7e7fe8a35539f81
praneeth <praneeth.nkl@gmail.com>
2023-10-12 23:41:41 +0530

    db configed

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors');
-require('dotenv').config();
-require('./config/db')();
+require("dotenv/config");
+
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME,
+  })
+  .then(() => {
+    console.log("Connected to Database!!!");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 const VehicleRoute = require('./routes/vechicle');
 
 app.use(cors()) 
